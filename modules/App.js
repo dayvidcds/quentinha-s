7902157824b16b5aa81dcf5e22befca499e1eede
dayvidcds <dayvid.clementino@gmail.com>
2017-11-13 14:29:47 -0300

    segurança das rotas 100% funcional com autenticação por token

--- modules/App.js
+++ modules/App.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var jwt = require('jsonwebtoken');
 
 var db = require('../Persistence/ConnectionDB');
 
@@ -18,14 +19,33 @@ var userBusiness = new UserBusiness(uRep);
 app.use('/user', routerUser);
 
 app.get('/', (req, res) => {
-    res.send('Bem Vindo!')
+    res.send('Bem vindo a API!')
 })
 
 // Inicio das rotas usuário
 
-routerUser.get('/findAll', (req, res) => {
-    userBusiness.findAllUsers().then((resp) => {
-        res.send(resp)
+routerUser.post('/login', (req, res) => {
+    userBusiness.login({
+        password: req.body.password,
+        cpf: req.body.cpf
+    }).then((resp) => {
+        if (resp) {
+            var jwtSecret = 'SECRET'
+            var token = jwt.sign(resp.toJSON(), jwtSecret, {
+                expiresIn: 1440 //o token irá expirar em 24 horas
+            });
+            res.json({
+                success: true,
+                message: 'Token criado!',
+                token: token
+            })
+        } else {
+            res.json({
+                success: false,
+                message: 'Token não criado!',
+                token: 'null'
+            })
+        }
     })
 })
 
@@ -41,12 +61,37 @@ routerUser.post('/insert', (req, res) => {
     })
 })
 
-routerUser.post('/login', (req, res) => {
-    userBusiness.login({
-        password: req.body.password,
-        cpf: req.body.cpf
-    }).then((resp) => {
-        res.send('LOGOU')
+routerUser.use((req, res, next) => {
+    var token = req.body.token || req.query.token || req.headers['x-access-token']
+
+    if (token) {
+        var jwtSecret = 'SECRET'
+        jwt.verify(token, jwtSecret, (err, decoded) => {
+            if (err) {
+                return res.json({
+                    success: false,
+                    message: 'Falha ao tentar autenticar o token!'
+                })
+            } else {
+                //se tudo correr bem, salver a requisição para o uso em outras rotas
+                req.decoded = decoded
+                next();
+            }
+        })
+
+    } else {
+        // se não tiver o token, retornar o erro 403
+        return res.status(403).send({
+            success: false,
+            message: 'Não há token.'
+        })
+    }
+})
+
+
+routerUser.get('/findAll', (req, res) => {
+    userBusiness.findAllUsers().then((resp) => {
+        res.send(resp)
     })
 })
 
@@ -56,6 +101,10 @@ routerUser.post('/remove', (req, res) => {
     ).then((resp) => {
         res.send('ok')
     })
+})
+
+routerUser.get('/home', (req, res) => {
+    res.send('BEM VINDO')
 })
 
 module.exports = app
