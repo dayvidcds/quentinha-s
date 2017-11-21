a8adc53913ec089200d3d6f2f6024226f41b6d90
sostenesg7 <sostenesg7@gmail.com>
2017-11-21 20:58:23 -0300

    script para exportar as collections

---
+++ mongo_export.sh
@@ -0,0 +1,3 @@
+mongoexport --db teste --collection foodsales --out foodsales.json
+mongoexport --db teste --collection restaurants --out restaurants.json
+mongoexport --db teste --collection users --out users.json
