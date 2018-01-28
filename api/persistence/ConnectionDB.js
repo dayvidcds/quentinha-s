97bf82caf1a1a6bfc6a22d8651b54361fbc30b64
dayvidcds <dayvid.clementino@gmail.com>
2018-01-28 20:37:18 -0300

    au

--- api/persistence/ConnectionDB.js
+++ api/persistence/ConnectionDB.js
@@ -3,18 +3,19 @@ const dbconfig = require('../config/db.json')
 let mongoConn = null
 
 mongoose.Promise = global.Promise;
+mongodb:
 
-(() => {
-    let error = ''
-    const url = 'mongodb://dayvidcds:dayvid12344223@ds249737.mlab.com:49737/levaeu' //'mongodb://' + dbconfig.address + ':' + dbconfig.port + '/' + dbconfig.db //const url = 'mongodb://dayvidcds:dayvid12344223@ds259245.mlab.com:59245/databasequentinhas'
-    mongoConn = mongoose.connect(url, { useMongoClient: true }, (err) => {
-        if (err) {
-            error = err
+    (() => {
+        let error = ''
+        const url = 'mongodb://dayvidcds:dayvid12344223@ds117858.mlab.com:17858/quentinhas' //'mongodb://' + dbconfig.address + ':' + dbconfig.port + '/' + dbconfig.db //const url = 'mongodb://dayvidcds:dayvid12344223@ds259245.mlab.com:59245/databasequentinhas'
+        mongoConn = mongoose.connect(url, { useMongoClient: true }, (err) => {
+            if (err) {
+                error = err
+            }
+        })
+        if (error !== '') {
+            throw Error('erro no db')
         }
-    })
-    if (error !== '') {
-        throw Error('erro no db')
-    }
-})()
+    })()
 
 module.exports = mongoConn
